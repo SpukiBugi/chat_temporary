eba52a288c65224dbb0664edcc93fb84e5e2e9d3
Valery Golenishchev <gv@idaproject.com>
2023-03-30 21:16:16 +0300

    feat(Chat) mob opening/closing

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
         :class="[$style.App, $style[`_${animationType}`]]"
         :style="styleList"
     >
-        <transition name="fade">
-            <div v-show="stepId"
+        <transition name="widget-sova-fade">
+            <div v-show="isOpen"
                  :class="$style.overlay"
                  @click="onClose"
             >
@@ -62,9 +62,9 @@
 </template>
 
 <script>
-import { gsap } from 'gsap/dist/gsap.js';
 import { debounce } from '@/assets/js/utils/common-utils';
 import testHistory from '@/assets/json/testHistory';
+import AppAnim from './AppAnim';
 
 import Menu from '@/components/app/Menu.vue';
 import Avatar from '@/components/ui/Avatar.vue';
@@ -94,6 +94,10 @@ export default {
         /* eslint-enable vue/no-unused-components */
     },
 
+    mixins: [
+        AppAnim,
+    ],
+
     props: {
         id: {
             type: String,
@@ -119,7 +123,7 @@ export default {
             message: '',
 
             /** Info */
-            history: [] || testHistory,
+            history: testHistory,
 
             animationType: 'bottom',
             debouncedResize: debounce(this.onResize, 100),
@@ -239,267 +243,6 @@ export default {
                 this.closeAnimBottom();
             }
         },
-
-        /** Анимации */
-
-        checkAnimationType() {
-            const menuRect = this.$refs.menu.$el.getBoundingClientRect();
-
-            if (menuRect?.top < window.innerHeight / 2) {
-                this.animationType = 'top';
-            } else {
-                this.animationType = 'bottom';
-            }
-        },
-
-        openAnimBottom() {
-            const duration = .4;
-            const timeline = gsap.timeline();
-
-            timeline.fromTo(this.$refs.avatar.$refs.status, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                duration: duration,
-            }, 0);
-
-            timeline.to(this.$refs.avatarWrap, {
-                opacity: 1,
-            }, 0);
-
-            timeline.to(this.$refs.menu.$el, {
-                opacity: 0,
-                duration: .3,
-            }, 0);
-
-            timeline.to(this.$refs.avatarWrap, {
-                duration: duration,
-                top: '8px',
-                left: '50%',
-                transform: 'scale(1.34) translate(-50%)',
-            }, 0);
-
-            timeline.to(this.$refs.mainWrap, {
-                duration: duration,
-                transform: 'scale(1)',
-                transformOrigin: '100% 100%',
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.head, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                duration: .3,
-                delay: duration,
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.componentWrap, {
-                opacity: 0,
-                transform: 'scale(.8)',
-            }, {
-                transform: 'scale(1)',
-                opacity: 1,
-                duration: .3,
-                delay: duration,
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.mainMenu.$el, {
-                opacity: 0,
-                transform: 'scale(.8)',
-            }, {
-                transform: 'scale(1)',
-                opacity: 1,
-                duration: .3,
-                delay: duration,
-            }, 0);
-        },
-
-        openAnimTop() {
-            const duration = .4;
-            const timeline = gsap.timeline();
-
-            timeline.fromTo(this.$refs.avatar.$refs.status, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                duration: .3,
-                delay: duration,
-            }, 0);
-
-            timeline.to(this.$refs.avatarWrap, {
-                opacity: 1,
-            }, 0);
-
-            timeline.set(this.$refs.menu.$el, {
-                opacity: 0,
-            }, 0);
-
-            timeline.to(this.$refs.avatarWrap, {
-                duration: duration,
-                top: 0,
-                left: '50%',
-                xPercent: -50,
-                y: 8,
-                transform: 'scale(1.34)',
-            }, 0);
-
-            timeline.to(this.$refs.mainContainer, {
-                duration: duration,
-                top: '100%',
-                yPercent: -100,
-            }, 0);
-
-            timeline.to(this.$refs.mainWrap, {
-                duration: duration,
-                transform: 'scale(1)',
-                transformOrigin: '50% 40px',
-                delay: duration,
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.head, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                duration: .3,
-                delay: duration * 2,
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.componentWrap, {
-                opacity: 0,
-                transform: 'scale(.8)',
-            }, {
-                transform: 'scale(1)',
-                opacity: 1,
-                duration: .3,
-                delay: duration * 2,
-            }, 0);
-
-            timeline.fromTo(this.$refs.main?.$refs.mainMenu.$el, {
-                opacity: 0,
-                transform: 'scale(.8)',
-            }, {
-                transform: 'scale(1)',
-                opacity: 1,
-                duration: .3,
-                delay: duration * 2,
-            }, 0);
-        },
-
-        closeAnimBottom() {
-            const duration = .4;
-            const firstStep = .1;
-            const timeline = gsap.timeline();
-
-            timeline.to(this.$refs.main?.$refs.head, {
-                opacity: 0,
-                duration: firstStep,
-            }, 0);
-
-            timeline.to(this.$refs.main?.$refs.componentWrap, {
-                transform: 'scale(.8)',
-                opacity: 0,
-                duration: firstStep,
-            }, 0);
-
-            timeline.to(this.$refs.main?.$refs.mainMenu.$el, {
-                opacity: 0,
-                transform: 'scale(.8)',
-                duration: firstStep,
-            }, 0);
-
-            timeline.to(this.$refs.mainWrap, {
-                duration: (duration - firstStep / 2),
-                transform: 'scale(0)',
-                transformOrigin: `${this.$refs.mainWrap.clientWidth - 30}px ${this.$refs.mainWrap.clientHeight - 30}px`,
-            }, firstStep);
-
-            timeline.to(this.$refs.avatar.$refs.status, {
-                duration: (duration - firstStep / 2),
-                opacity: 0,
-            }, firstStep);
-
-            timeline.to(this.$refs.avatarWrap, {
-                duration: (duration - firstStep / 2),
-                top: 'calc(100% - 8px)',
-                left: 'calc(100% - 8px)',
-                transform: 'scale(1) translate3d(-100%, -100%, 0)',
-            }, firstStep);
-
-            timeline.set(this.$refs.menu.$refs.mainBlur, {
-                opacity: 0,
-            }, firstStep);
-
-            timeline.set(this.$refs.menu.$el, {
-                opacity: 1,
-            }, duration);
-
-            timeline.set(this.$refs.menu.$refs.mainBlur, {
-                opacity: 1,
-            }, duration);
-
-            timeline.set(this.$refs.avatarWrap, {
-                opacity: 0,
-            }, duration);
-
-            timeline.set(this, {
-                stepId: '',
-            }, duration);
-        },
-
-        closeAnimTop() {
-            const duration = .4;
-            const timeline = gsap.timeline();
-            const menuRect = this.$refs.menu.$el.getBoundingClientRect();
-
-            timeline.to(this.$refs.mainWrap, {
-                duration: .15,
-                transform: 'scale(0)',
-                transformOrigin: '50% 40px',
-            }, 0);
-
-            timeline.to(this.$refs.avatar.$refs.status, {
-                duration: .15,
-                opacity: 0,
-            }, 0);
-
-            timeline.to(this.$refs.mainContainer, {
-                duration: duration - .1,
-                top: menuRect?.top,
-                yPercent: 0,
-                onComplete: () => this.$refs.mainContainer.style.top = '',
-            }, .1);
-
-            timeline.to(this.$refs.avatarWrap, {
-                duration: duration - .1,
-                top: 0,
-                left: 'calc(100% - 20px)',
-                y: 8,
-                xPercent: -100,
-                transform: 'scale(1)',
-            }, .1);
-
-            timeline.set(this.$refs.menu.$refs.mainBlur, {
-                opacity: 0,
-            }, 0);
-
-            timeline.set(this.$refs.menu.$el, {
-                opacity: 1,
-            }, duration);
-
-            timeline.set(this.$refs.menu.$refs.mainBlur, {
-                opacity: 1,
-            }, duration);
-
-            timeline.set(this.$refs.avatarWrap, {
-                opacity: 0,
-            }, duration);
-
-            timeline.set(this, {
-                stepId: '',
-            }, duration);
-        },
-
-        /** Конец Анимации */
 
         /** Чат */
         async onSubmit() {
---
+++ src/AppAnim.js
@@ -0,0 +1,321 @@
+import { gsap } from 'gsap/dist/gsap.js';
+
+export default {
+    methods: {
+        checkAnimationType() {
+            const menuRect = this.$refs.menu.$el.getBoundingClientRect();
+
+            if (menuRect?.top < window.innerHeight / 2) {
+                this.animationType = 'top';
+            } else {
+                this.animationType = 'bottom';
+            }
+        },
+
+        openAnimBottom() {
+            const duration = .4;
+            const timeline = gsap.timeline();
+
+            /** Иконка статуса на аватарке */
+            timeline.fromTo(this.$refs.avatar.$refs.status, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: duration,
+            }, 0);
+            //
+
+            /** Подмена аватарки */
+            timeline.to(this.$refs.avatarWrap, {
+                opacity: 1,
+            }, 0);
+
+            timeline.to(this.$refs.menu.$el, {
+                opacity: 0,
+                duration: .3,
+            }, 0);
+            //
+
+            /** Раскрытие контента */
+            timeline.to(this.$refs.mainWrap, {
+                duration: duration,
+                transform: 'scale(1)',
+                transformOrigin: '100% 100%',
+            }, 0);
+            //
+
+            /** Перемещение аватарки */
+            timeline.to(this.$refs.avatarWrap, {
+                duration: duration,
+                top: '8px',
+                left: '50%',
+                transform: 'scale(1.34) translate(-50%)',
+            }, 0);
+            //
+
+            /** Анимация внутренних элементов чата */
+            timeline.fromTo(this.$refs.main?.$refs.head, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: .3,
+                delay: duration,
+            }, 0);
+
+            timeline.fromTo(this.$refs.main?.$refs.componentWrap, {
+                opacity: 0,
+                transform: 'scale(.8)',
+            }, {
+                transform: 'scale(1)',
+                opacity: 1,
+                duration: .3,
+                delay: duration,
+            }, 0);
+
+            timeline.fromTo(this.$refs.main?.$refs.mainMenu.$el, {
+                opacity: 0,
+                transform: 'scale(.8)',
+            }, {
+                transform: 'scale(1)',
+                opacity: 1,
+                duration: .3,
+                delay: duration,
+            }, 0);
+            //
+        },
+
+        openAnimTop() {
+            const duration = .4;
+            const timeline = gsap.timeline();
+
+            /** Иконка статуса на аватарке */
+            timeline.fromTo(this.$refs.avatar.$refs.status, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: .3,
+                delay: duration,
+            }, 0);
+            //
+
+            /** Подмена аватарки */
+            timeline.to(this.$refs.avatarWrap, {
+                opacity: 1,
+            }, 0);
+
+            timeline.set(this.$refs.menu.$el, {
+                opacity: 0,
+            }, 0);
+            //
+
+            /** Раскрытие контента */
+            timeline.to(this.$refs.mainWrap, {
+                duration: duration,
+                transform: 'scale(1)',
+                transformOrigin: '50% 40px',
+                delay: duration,
+            }, 0);
+            //
+
+            /** Перемещение аватарки */
+            timeline.to(this.$refs.mainContainer, {
+                duration: duration,
+                top: this.history.length ? 12 : '100%',
+                yPercent: this.history.length ? 0: -100,
+            }, 0);
+
+            timeline.to(this.$refs.avatarWrap, {
+                duration: duration,
+                top: 0,
+                left: '50%',
+                xPercent: -50,
+                y: 8,
+                transform: 'scale(1.34)',
+            }, 0);
+            //
+
+            /** Анимация внутренних элементов чата */
+            timeline.fromTo(this.$refs.main?.$refs.head, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: .3,
+                delay: duration * 2,
+            }, 0);
+
+            timeline.fromTo(this.$refs.main?.$refs.componentWrap, {
+                opacity: 0,
+                transform: 'scale(.8)',
+            }, {
+                transform: 'scale(1)',
+                opacity: 1,
+                duration: .3,
+                delay: duration * 2,
+            }, 0);
+
+            timeline.fromTo(this.$refs.main?.$refs.mainMenu.$el, {
+                opacity: 0,
+                transform: 'scale(.8)',
+            }, {
+                transform: 'scale(1)',
+                opacity: 1,
+                duration: .3,
+                delay: duration * 2,
+            }, 0);
+            //
+        },
+
+        closeAnimBottom() {
+            const duration = .4;
+            const firstStep = .1;
+            const timeline = gsap.timeline();
+
+            /** Анимация внутренних элементов чата */
+            timeline.to(this.$refs.main?.$refs.head, {
+                opacity: 0,
+                duration: firstStep,
+            }, 0);
+
+            timeline.to(this.$refs.main?.$refs.componentWrap, {
+                transform: 'scale(.8)',
+                opacity: 0,
+                duration: firstStep,
+            }, 0);
+
+            timeline.to(this.$refs.main?.$refs.mainMenu.$el, {
+                opacity: 0,
+                transform: 'scale(.8)',
+                duration: firstStep,
+            }, 0);
+            //
+
+            /** Иконка статуса на аватарке */
+            timeline.to(this.$refs.avatar.$refs.status, {
+                duration: (duration - firstStep / 2),
+                opacity: 0,
+            }, firstStep);
+            //
+
+            /** Раскрытие контента */
+            timeline.to(this.$refs.mainWrap, {
+                duration: (duration - firstStep / 2),
+                transform: 'scale(0)',
+                transformOrigin: `${this.$refs.mainWrap.clientWidth - 30}px ${this.$refs.mainWrap.clientHeight - 30}px`,
+            }, firstStep);
+            //
+
+            /** Перемещение аватарки */
+            timeline.to(this.$refs.avatarWrap, {
+                duration: (duration - firstStep / 2),
+                top: 'calc(100% - 8px)',
+                left: 'calc(100% - 8px)',
+                transform: 'scale(1) translate3d(-100%, -100%, 0)',
+            }, firstStep);
+            //
+
+            /** Подмена аватарки */
+            timeline.set(this.$refs.menu.$refs.mainBlur, {
+                opacity: 0,
+            }, firstStep);
+
+            timeline.set(this.$refs.menu.$el, {
+                opacity: 1,
+            }, duration);
+
+            timeline.set(this.$refs.menu.$refs.mainBlur, {
+                opacity: 1,
+            }, duration);
+
+            timeline.set(this.$refs.avatarWrap, {
+                opacity: 0,
+            }, duration);
+            //
+
+            timeline.set(this, {
+                stepId: '',
+            }, duration);
+        },
+
+        closeAnimTop() {
+            const duration = .5;
+            const firstStep = .1;
+            const secondStep = .15;
+            const timeline = gsap.timeline();
+            const menuRect = this.$refs.menu.$el.getBoundingClientRect();
+
+            /** Анимация внутренних элементов чата */
+            timeline.to(this.$refs.main?.$refs.head, {
+                opacity: 0,
+                duration: firstStep,
+            }, 0);
+
+            timeline.to(this.$refs.main?.$refs.componentWrap, {
+                transform: 'scale(.8)',
+                opacity: 0,
+                duration: firstStep,
+            }, 0);
+
+            timeline.to(this.$refs.main?.$refs.mainMenu.$el, {
+                opacity: 0,
+                transform: 'scale(.8)',
+                duration: firstStep,
+            }, 0);
+            //
+
+            /** Иконка статуса на аватарке */
+            timeline.to(this.$refs.avatar.$refs.status, {
+                duration: secondStep,
+                opacity: 0,
+            }, firstStep - .1);
+            //
+
+            /** Раскрытие контента */
+            timeline.to(this.$refs.mainWrap, {
+                duration: secondStep,
+                transform: 'scale(0)',
+                transformOrigin: '50% 40px',
+            }, firstStep - .1);
+            //
+
+            /** Перемещение аватарки */
+            timeline.to(this.$refs.mainContainer, {
+                duration: duration - (firstStep + secondStep),
+                top: menuRect?.top,
+                yPercent: 0,
+                onComplete: () => this.$refs.mainContainer.style.top = '',
+            }, (firstStep - .1) + secondStep);
+
+            timeline.to(this.$refs.avatarWrap, {
+                duration: duration - (firstStep + secondStep),
+                top: 0,
+                left: 'calc(100% - 20px)',
+                y: 8,
+                xPercent: -100,
+                transform: 'scale(1)',
+            }, (firstStep - .1) + secondStep);
+            //
+
+            /** Подмена аватарки */
+            timeline.set(this.$refs.menu.$refs.mainBlur, {
+                opacity: 0,
+            }, 0);
+
+            timeline.set(this.$refs.menu.$el, {
+                opacity: 1,
+            }, duration);
+
+            timeline.set(this.$refs.menu.$refs.mainBlur, {
+                opacity: 1,
+            }, duration);
+
+            timeline.set(this.$refs.avatarWrap, {
+                opacity: 0,
+            }, duration);
+            //
+
+            timeline.set(this, {
+                stepId: '',
+            }, duration);
+        },
+    },
+};
